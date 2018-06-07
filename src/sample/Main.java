955940c811c2055e139e1dd7cc598e64b9650843
Rommel Rico <me@rommelrico.com>
2018-06-07 08:37:38 -0700

    Setting up a button

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,7 +13,13 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+
+        // Adding a button.
+        Button okButton = new Button("OK");
+        Scene scene = new Scene(okButton, 200, 250);
+
+        // Displaying the scene
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
