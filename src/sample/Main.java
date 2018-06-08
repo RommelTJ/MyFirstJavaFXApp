4a028e9ec59f226adf0871600a40786bffea6e71
Rommel Rico <me@rommelrico.com>
2018-06-08 06:45:50 -0700

    Adding a circle

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,6 +3,9 @@ package sample;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.shape.Circle;
+import javafx.scene.paint.Color;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.scene.layout.StackPane;
 
@@ -24,6 +27,21 @@ public class Main extends Application {
         Scene secondScene = new Scene(secondButton, 200, 250);
         newStage.setScene(secondScene);
         newStage.show();
+
+        // Third stage
+        Stage thirdStage = new Stage();
+        Circle circle = new Circle();
+        circle.setCenterX(100);
+        circle.setCenterY(100);
+        circle.setRadius(50);
+        circle.setStroke(Color.LAVENDER);
+        circle.setFill(Color.RED);
+        Pane pane1 = new Pane();
+        pane1.getChildren().add(circle);
+        Scene thirdScene = new Scene(pane1, 200, 200);
+        thirdStage.setTitle("Circle Demo");
+        thirdStage.setScene(thirdScene);
+        thirdStage.show();
     }
 
 
