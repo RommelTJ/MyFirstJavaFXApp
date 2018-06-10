d01618f9f0ae1b3d6e9958583b1a300d57795fa2
Rommel Rico <rommeltj@gmail.com>
2018-06-09 17:11:46 -0700

    ShowPanes class

---
+++ src/com/rommelrico/sample/ShowPanes.java
@@ -0,0 +1,24 @@
+package com.rommelrico.sample;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+import static javafx.application.Application.launch;
+
+public class ShowPanes extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
