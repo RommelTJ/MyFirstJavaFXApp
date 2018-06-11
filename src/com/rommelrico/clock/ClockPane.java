9666e4c93cee5165d39f5a55c4caeeb76cfe050a
Rommel Rico <rommeltj@gmail.com>
2018-06-10 21:00:05 -0700

    ClockPane class

---
+++ src/com/rommelrico/clock/ClockPane.java
@@ -0,0 +1,16 @@
+package com.rommelrico.clock;
+
+public class ClockPane {
+
+    public String getHour() {
+        return null;
+    }
+
+    public String getMinute() {
+        return null;
+    }
+
+    public String getSecond() {
+        return null;
+    }
+}
