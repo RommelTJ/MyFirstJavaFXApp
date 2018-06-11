bc02da3944f81a7a0943d5de95702df9569b1c1a
Rommel Rico <rommeltj@gmail.com>
2018-06-10 19:54:32 -0700

    Working with the Text class

--- src/com/rommelrico/sample/ShowShapes.java
+++ src/com/rommelrico/sample/ShowShapes.java
@@ -1,13 +1,26 @@
 package com.rommelrico.sample;
 
+import com.sun.javafx.css.FontFace;
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class ShowShapes extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        Pane pane = new Pane();
+        pane.setPadding(new Insets(5));
+        Text myText = new Text(20, 20, "Programming is fun");
+        myText.setFont(Font.font("Courier", FontWeight.BOLD, FontPosture.ITALIC, 15));
+        pane.getChildren().add(myText);
 
+        
     }
 
     public static void main(String[] args) {
