f67dc2881ba3b9387871c86885b6c4ed51eee88d
Rommel Rico <rommeltj@gmail.com>
2018-06-10 19:50:32 -0700

    ShowShapes Class

---
+++ src/com/rommelrico/sample/ShowShapes.java
@@ -0,0 +1,17 @@
+package com.rommelrico.sample;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ShowShapes extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
