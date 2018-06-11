bfb15957af9c92527dec85e4a58c6cd0dd5bebc2
Rommel Rico <rommeltj@gmail.com>
2018-06-10 19:59:24 -0700

    Working with the Text class directly

--- src/com/rommelrico/sample/ShowShapes.java
+++ src/com/rommelrico/sample/ShowShapes.java
@@ -4,6 +4,7 @@ import com.sun.javafx.css.FontFace;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
@@ -22,7 +23,12 @@ public class ShowShapes extends Application {
 
         Text myText2 = new Text(60, 60, "Programming is fun\n Display Text");
         pane.getChildren().add(myText2);
-        
+
+        Text myText3 = new Text(10, 100, "Programming is fun\n Display Text");
+        myText3.setFill(Color.RED);
+        myText3.setUnderline(true);
+        myText3.setStrikethrough(true);
+        pane.getChildren().add(myText3);
     }
 
     public static void main(String[] args) {
