f5042117cfe07fbfa0c4d3f45f6c7fd5cc045808
Rommel Rico <rommeltj@gmail.com>
2018-06-11 06:22:27 -0700

    Removing unused import

--- src/com/rommelrico/clock/DisplayClock.java
+++ src/com/rommelrico/clock/DisplayClock.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
